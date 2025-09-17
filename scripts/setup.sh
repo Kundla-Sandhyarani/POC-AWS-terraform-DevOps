3eb190372665acbd456bd9f2d459ab2e8a510779
Kundla-Sandhyarani <kundlasandhya@gmail.com>
2025-09-17 16:14:39 +0530

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -28,4 +28,40 @@ cd /opt/tomcat
 sudo wget https://downloads.apache.org/tomcat/tomcat-9/v9.0.80/bin/apache-tomcat-9.0.80.tar.gz
 sudo tar xzvf apache-tomcat-9.0.80.tar.gz --strip-components=1
 sudo chmod +x /opt/tomcat/bin/*.sh
+sudo sed -i 's/port="8080"/port="9090"/' /opt/tomcat/conf/server.xml
 sudo /opt/tomcat/bin/startup.sh
+
+# Create a simple Java web app
+mkdir -p ~/java-webapp/src/main/webapp
+mkdir -p ~/java-webapp/WEB-INF
+
+cat > ~/java-webapp/src/main/webapp/index.jsp <<EOF
+<html>
+  <body>
+    <h1>Hello from Dockerized WAR!</h1>
+  </body>
+</html>
+EOF
+
+cat > ~/java-webapp/WEB-INF/web.xml <<EOF
+<web-app xmlns="http://java.sun.com/xml/ns/javaee" version="3.0">
+  <display-name>SampleApp</display-name>
+</web-app>
+EOF
+
+# Package WAR file
+cd ~/java-webapp
+mkdir -p target
+jar -cvf target/sample.war -C src/main/webapp/ . -C WEB-INF/ .
+
+# Create Dockerfile to deploy WAR in Tomcat
+cat > Dockerfile <<EOF
+FROM tomcat:9.0
+COPY target/sample.war /usr/local/tomcat/webapps/sample.war
+EOF
+
+# Build Docker image
+docker build -t sample-java-app .
+
+# Run Docker container
+docker run -d --name sample-app -p 8081:8080 sample-java-app
