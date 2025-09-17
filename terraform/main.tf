1fb7735449d34bbc03bf8b3fe7a00636fd062698
Kundla-Sandhyarani <kundlasandhya@gmail.com>
2025-09-17 16:16:36 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "devops_server" {
-  ami           = "ami-0059ed5a3aacdfe15" # LINUX AMI
+  ami           = "ami-0059ed5a3aacdfe15" # Amazon Linux AMI
   instance_type = "t2.micro"
   key_name      = var.key_name
 
@@ -18,9 +18,10 @@ resource "aws_instance" "devops_server" {
 
 resource "aws_security_group" "devops_sg" {
   name        = "devops_sg"
-  description = "Allow SSH, Jenkins, Tomcat"
+  description = "Allow SSH, Jenkins, Tomcat (Docker)"
 
   ingress {
+    description = "SSH Access"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
@@ -28,6 +29,7 @@ resource "aws_security_group" "devops_sg" {
   }
 
   ingress {
+    description = "Jenkins UI"
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
@@ -35,13 +37,23 @@ resource "aws_security_group" "devops_sg" {
   }
 
   ingress {
+    description = "Dockerized Java App"
     from_port   = 8081
     to_port     = 8081
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "Tomcat (native)"
+    from_port   = 9090
+    to_port     = 9090
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
+    description = "Allow all outbound traffic"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
