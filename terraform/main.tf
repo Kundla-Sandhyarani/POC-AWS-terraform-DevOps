33b7b280addd289f804689abc813d7accf4159a2
Kundla-Sandhyarani <kundlasandhya@gmail.com>
2025-09-18 17:50:41 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "devops_server" {
   ami           = "ami-0059ed5a3aacdfe15" # Amazon Linux AMI
   instance_type = "t2.micro"
-  key_name      = var.key_name
+  key_name      = "sandhya-key"
 
   user_data = file("${path.module}/../scripts/setup.sh")
 
