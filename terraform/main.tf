f1e50e45667ba6d233bf74dc7604787128908547
Kundla-Sandhyarani <kundlasandhya@gmail.com>
2025-09-19 10:07:04 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,13 +4,13 @@ provider "aws" {
 
 resource "aws_instance" "devops_server" {
   ami           = "ami-0059ed5a3aacdfe15" # Amazon Linux AMI
-  instance_type = "t2.micro"
+  instance_type = "t3.micro"
   key_name      = var.key_name
 
   user_data = file("${path.module}/../scripts/setup.sh")
 
   tags = {
-    Name = "DevOpsPOC"
+    Name = "SanDevOpsPOC"
   }
 
   vpc_security_group_ids = [aws_security_group.devops_sg.id]
